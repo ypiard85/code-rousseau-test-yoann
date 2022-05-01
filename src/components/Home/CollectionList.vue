6f68d65e5228a08dc22a27a5c8f856d6c4255993
ypiard85 <60528207+ypiard85@users.noreply.github.com>
2022-05-01 13:30:41 +0200

    add search form - home page

--- src/components/Home/CollectionList.vue
+++ src/components/Home/CollectionList.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+      <input type="text" v-model="query" class="form-control mb-5" placeholder="Recherchez une collection"  />
         <table class="table">
             <thead>
           <tr>
@@ -10,11 +11,11 @@
           </tr>
         </thead>
         <tbody>
-            <tr v-for="collection in this.collections" :key="collection.id">
+            <tr v-for="collection in handleFilter" :key="collection.id">
               <th scope="row"><img width="25" :src="collection.icon_svg_uri" alt=""></th>
               <td><span class="fw-bold">{{ collection.name }}</span><span class="text-secondary ms-2">{{ collection.code }}</span> </td>
               <td> {{ collection.released_at }} </td>
-              <td><router-link :to="'/collection/' + collection.code " class="btn btn-sm btn-primary"> Voir la col</router-link></td>
+              <td><router-link :to="'/collection/' + collection.code " class="btn btn-sm btn-primary"> Voir plus</router-link></td>
             </tr>
         </tbody>
       </table>
@@ -33,6 +34,18 @@
     props:{
       collections : Array
     },
+
+    data(){
+      return{
+        query: ""
+      }
+    },
+
+    computed:{
+            handleFilter(){
+               return this.collections.filter(c => c.name.toLowerCase().includes(this.query.toLowerCase()))
+            },
+        }
 
   }
 </script>
